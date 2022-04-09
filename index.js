5d5ccabf1a804fd35e8676c7de5d015d9e51fbbc
mhlamvu <bafana.mhlamvu@gmail.com>
2022-04-09 23:25:37 +0200

    added mailing list

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.post("/", (req, res) => {
-  console.log(req.body)
+  console.log(req.body.email)
   res.send("Success!!!")
 })
 
