1f6107654872507668f797f1877da5475464836d
mhlamvu <bafana.mhlamvu@gmail.com>
2022-04-10 02:38:42 +0200

    connected backend using axios to mailchimp

--- index.js
+++ index.js
@@ -11,10 +11,49 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.post("/", (req, res) => {
-  console.log(req.body.email)
+  addEmailToMailchimp(req.body.email)
   res.send("Success!!!")
 })
 
 app.listen(3000, () => {
   console.log("Server running on port 3000")
 })
+
+
+const addEmailToMailchimp = email => {
+  const axios = require("axios").default;
+
+  const options = {
+    method: 'POST',
+    url: 'https://us14.api.mailchimp.com/3.0/lists/74c1740867/members',
+    headers: {
+      'user-agent': 'vscode-restclient',
+      'content-type': 'application/application/json',
+      authorization: 'token 559214b542dd914f437b30ace69cdb0c-us14'
+    },
+    data: {
+      "email_address": email,
+      "status": "subscribed"
+    }
+  };
+
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+
+}
+
+
+
+
+/*
+
+curl --request GET \
+--url 'https://us14.api.mailchimp.com/3.0/' \
+--user 'anystring:559214b542dd914f437b30ace69cdb0c-us14
+
+API: 559214b542dd914f437b30ace69cdb0c-us14
+
+*/
